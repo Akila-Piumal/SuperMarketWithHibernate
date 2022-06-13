ef87478850b8b3f8ff213f524a0ff71909bf68f5
Akila-Piumal <akilapiumal16@gmail.com>
2022-06-13 16:56:01 +0530

    Begin the transaction and add the commit command of the transaction

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,6 +1,7 @@
 import lk.ijse.super_market_hibernate.entity.Customer;
 import lk.ijse.super_market_hibernate.util.FactoryConfiguration;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class AppInitializer {
     public static void main(String[] args) {
@@ -13,7 +14,11 @@ public class AppInitializer {
 
         Session session = FactoryConfiguration.getInstance().getSession();
 
+        Transaction transaction = session.beginTransaction();
 
+
+
+        transaction.commit();
 
         session.close();
     }
