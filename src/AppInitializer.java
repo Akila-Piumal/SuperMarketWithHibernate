62551b56f2cad18af74560081c4ab0481fd2d677
Akila-Piumal <akilapiumal16@gmail.com>
2022-06-13 17:08:50 +0530

    Updated a customer

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -12,11 +12,19 @@ public class AppInitializer {
         c1.setAddress("Mathugama");
         c1.setSalary(90000);
 
+        Customer c2=new Customer();
+        c2.setId("C001");
+        c2.setName("Akila");
+        c2.setAddress("Kaluthara");
+        c2.setSalary(100000);
+
         Session session = FactoryConfiguration.getInstance().getSession();
 
         Transaction transaction = session.beginTransaction();
 
-        session.save(c1);
+        //session.save(c1);
+
+        session.update(c2);
 
         transaction.commit();
 
