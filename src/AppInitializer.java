22478b398984351779c1437ae5ee25c5671afbbd
Akila-Piumal <akilapiumal16@gmail.com>
2022-06-13 17:35:39 +0530

    search and deleted a customer

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -5,14 +5,14 @@ import org.hibernate.Transaction;
 
 public class AppInitializer {
     public static void main(String[] args) {
-        Customer c1=new Customer();
+        Customer c1 = new Customer();
 
         c1.setId("C001");
         c1.setName("Akila");
         c1.setAddress("Mathugama");
         c1.setSalary(90000);
 
-        Customer c2=new Customer();
+        Customer c2 = new Customer();
         c2.setId("C001");
         c2.setName("Akila");
         c2.setAddress("Kaluthara");
@@ -22,9 +22,18 @@ public class AppInitializer {
 
         Transaction transaction = session.beginTransaction();
 
+        // Save Customer
         //session.save(c1);
 
-        session.update(c2);
+        // Update Customer
+        //session.update(c2);
+
+        // search Customer
+        // Customer customer = session.get(Customer.class, "C001");
+        // System.out.println(customer);
+
+        // delete Customer
+        // session.delete(customer);
 
         transaction.commit();
 
